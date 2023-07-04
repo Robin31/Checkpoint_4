1a8869c81a8a5719a5fa24a03042b4a6930e05fc
Robin31 <robindenis@hotmail.fr>
2023-07-04 21:46:28 +0200

    Ajout routes pour les races

--- backend/src/router.js
+++ backend/src/router.js
@@ -10,4 +10,12 @@ router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
 
+const racesControllers = require("./controllers/racesControllers");
+
+router.get("/races", racesControllers.browse);
+router.get("/races/:id", racesControllers.read);
+router.put("/races/:id", racesControllers.edit);
+router.post("/races", racesControllers.add);
+router.delete("/races/:id", racesControllers.destroy);
+
 module.exports = router;
