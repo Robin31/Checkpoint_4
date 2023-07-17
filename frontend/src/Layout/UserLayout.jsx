5f3b3e45f0ec00a5ea08e45b8af88788b677baa4
Robin31 <robindenis@hotmail.fr>
2023-07-18 00:41:21 +0200

    debut de benevole

--- frontend/src/Layout/UserLayout.jsx
+++ frontend/src/Layout/UserLayout.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/NavbarCard/Navbar";
-// import Connect from "../components/Connect";
 
 function UserLayout() {
   return (
     <div>
       <Navbar />
-      {/* <Connect /> */}
       <Outlet />
     </div>
   );
