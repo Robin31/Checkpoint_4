fe949a58e7e3f1fa1f3fb9dd17346d220febd11f
Robin31 <robindenis@hotmail.fr>
2023-07-19 22:26:14 +0200

    Ajout de la page home et son css

--- frontend/src/pages/User/Acceuil.jsx
+++ frontend/src/pages/User/Acceuil.jsx
@@ -25,6 +25,26 @@ function Acceuil() {
       </div>
       <div className="acceuil__chiffres">
         <h2 className="acceuil__soustitre">Quelques chiffres</h2>
+        <div className="chiffre-container">
+          <div className="chiffre-card">
+            <h3 className="chiffre-titre">100.000</h3>
+            <p>
+              Soit le nombre de d'abandon, chaque année, d'animaux de compagnie
+              dans notre beau pays. Et on se dit civilisé ?
+            </p>
+          </div>
+          <div className="chiffre-card">
+            <h3 className="chiffre-titre">74j 3h et 26min</h3>
+            <p>C'est le temps moyen que nos chiens passe chez nous.</p>
+          </div>
+          <div className="chiffre-card">
+            <h3 className="chiffre-titre">62ans</h3>
+            <p>
+              Soit la durée de séjour de Brigitte chez nous... Personne n'en
+              veux, elle est immortelle.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
